4877b3cb4af2b84b3fa170c3b483e533c12b0dcf
Austin Moore <austinymoore@gmail.com>
2016-03-29 17:01:38 -0400

    finished Angular2.html

--- src/BowerStarter/wwwroot/js/scope.js
+++ src/BowerStarter/wwwroot/js/scope.js
@@ -11,3 +11,20 @@ personApp.controller('personController2', function ($scope) {
     $scope.firstName = 'Mary';
     $scope.lastName = 'Jane';
 });
+
+personApp.controller('personController3', function ($scope, personFactory) {
+    $scope.name = personFactory.getName();
+})
+
+// factory
+personApp.factory('personFactory', function () {
+    function getName() {
+        return 'Mary Jane';
+    }
+
+    var service = {
+        getName: getName
+    };
+
+    return service;
+})
