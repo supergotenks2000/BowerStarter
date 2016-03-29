bf0234ad42e5fd0c0aeedbd41c8d5806da7adfd4
Austin Moore <austinymoore@gmail.com>
2016-03-29 18:10:51 -0400

    added Angular3.html and stuffs

---
+++ src/BowerStarter/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using BowerStarter.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BowerStarter.Controllers
+{
+    public class PersonController : Controller
+    {
+        [Route("/api/people")]
+        public JsonResult GetPeople()
+        {
+            var people = new List<Person>()
+            {
+                new Person { Id = 1, FirstName = "John", LastName = "Doe" },
+                new Person { Id = 2, FirstName = "Mary", LastName = "Jane" },
+                new Person { Id = 3, FirstName = "Bob", LastName = "Parker" }
+            };
+            return Json(people);
+        }
+    }
+}
